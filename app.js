8695d47f2181b25c53716b01278c7e9369e0f96f
nick <nick@ccserver.local>
2018-11-03 19:12:42 -0400

    Make it work with botnet

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((req,res,next)=>{
 		next();
 	}
 });*/
-app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use((req,res,next)=>{
 	let now = new Date().toString();
